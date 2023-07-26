75f1d7ae75e311bff24473920e2370cccac48c09
MesfinGithinji <meshgithinji@gmail.com>
2023-07-27 02:21:20 +0300

    Describe tables

---
+++ show_metadata.sql
@@ -0,0 +1,5 @@
+-- query that shows metadata about the columns, indexes 
+-- and data partitions of each table in our database
+-- so you could to it all at once in one line
+
+DESCRIBE city; DESCRIBE country; DESCRIBE countrylanguage;
