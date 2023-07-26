541844d15e7d2e8288ff67a592d52bf281f3eb6c
MesfinGithinji <meshgithinji@gmail.com>
2023-07-27 02:19:07 +0300

    Use of Select Statements

---
+++ fetch_data.sql
@@ -0,0 +1,6 @@
+-- query that Fetches all the fields in a table 
+-- (limit the data to 20 rows,our dataset is a bit large)
+
+SELECT * FROM city LIMIT 20;
+SELECT * FROM country LIMIT 20;
+SELECT * FROM countrylanguage LIMIT 20;
