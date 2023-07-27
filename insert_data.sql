d42d815bca0abf86bc58062aa1098741bc61baf5
MesfinGithinji <meshgithinji@gmail.com>
2023-07-28 02:01:34 +0300

    Also use of insert into

---
+++ insert_data.sql
@@ -0,0 +1,9 @@
+-- query inserts multiple rows of data into a database table
+
+INSERT INTO city (ID,Name,CountryCode,District,Population)
+VALUES	
+	(18885 , 'Marsabit', 'KEN' , 'North Eastern' ,67567),
+	(18886 , 'Narok', 'KEN' , 'Rift Valley' ,112345),
+	(18887 , 'Siaya', 'KEN' , 'Nyanza', 693847),
+	(18888 , 'Matuu', 'KEN' , 'Eastern' , 576849),
+	(18889 , 'Kiambu', 'KEN' , 'Central' , 94567);
